0e66b6c100359da568332444bf999563509c7492
Maximiliano Osorio <maxiosorio@gmail.com>
2024-12-04 12:45:12 -0300

    add: login

---
+++ src/app/Login/Login.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useTapisConfig } from '@tapis/tapisui-hooks';
+import Loading from '../common/Loading/Loading';
+
+const Login: React.FC = () => {
+  const { accessToken } = useTapisConfig();
+
+  useEffect(() => {
+    if (accessToken?.access_token) {
+      window.location.href = '/';
+    } else {
+      const callbackUrl = `${window.location.origin}/oauth2/callback`;
+      const oauthClientId = 'vite-react-dev';
+      const targetUrl = `https://portals.tapis.io/v3/oauth2/login?redirect_uri=${encodeURIComponent(
+        callbackUrl,
+      )}&response_type=token&client_id=${oauthClientId}`;
+      window.location.href = targetUrl;
+    }
+  }, [accessToken]);
+
+  return <Loading loadingMessage="Logging in..." />;
+};
+
+export default Login;
