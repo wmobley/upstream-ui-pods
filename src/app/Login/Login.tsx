d6d3525401064bbd0d6e59fe9880f1f667c7416b
Maximiliano Osorio <maxiosorio@gmail.com>
2024-12-04 12:51:00 -0300

    fix: change client id

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
       window.location.href = '/';
     } else {
       const callbackUrl = `${window.location.origin}/oauth2/callback`;
-      const oauthClientId = 'vite-react-dev';
+      const oauthClientId = 'dso-tacc-netlify';
       const targetUrl = `https://portals.tapis.io/v3/oauth2/login?redirect_uri=${encodeURIComponent(
         callbackUrl,
       )}&response_type=token&client_id=${oauthClientId}`;
