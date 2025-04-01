33a1862fa914c79031af1d08ec18eb57454bc595
Maximiliano Osorio <maxiosorio@gmail.com>
2025-04-01 15:05:41 -0300

    fix: remove tapis references

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import { createRoot } from 'react-dom/client';
-import { TapisProvider } from '@tapis/tapisui-hooks';
-import { resolveBasePath } from './utils/resolveBasePath.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TapisProvider basePath={resolveBasePath()}>
-      <Router>
-        <App />
-      </Router>
-    </TapisProvider>
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 );
