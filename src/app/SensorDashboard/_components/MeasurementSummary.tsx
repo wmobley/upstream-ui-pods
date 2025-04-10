b5edbac10815e8192b343feb841180aa7fb6895b
Maximiliano Osorio <maxiosorio@gmail.com>
2025-04-10 14:05:05 -0400

    refactor: simplify LineConfidenceViz layout by integrating MeasurementSummary component
    - Removed individual measurement detail displays and replaced them with a MeasurementSummary component for improved code organization and readability.
    - Updated the layout to maintain a responsive design while enhancing the visual representation of sensor data.

---
+++ src/app/SensorDashboard/_components/MeasurementSummary.tsx
@@ -0,0 +1,111 @@
+import {
+  FaChartBar,
+  FaChartLine,
+  FaClock,
+  FaRuler,
+  FaChartPie,
+} from 'react-icons/fa6';
+import { formatNumber } from '../../common/NumberFormatter/NumberFortatterUtils';
+import { GetSensorResponse } from '@upstream/upstream-api';
+import { FaCalendarAlt } from 'react-icons/fa';
+
+interface MeasurementSummaryProps {
+  data: GetSensorResponse;
+}
+
+export const MeasurementSummary = ({ data }: MeasurementSummaryProps) => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">{data.variablename}</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6 w-full max-w-4xl">
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaRuler className="text-green-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Alias</p>
+            <p className="font-medium">{data.alias}</p>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaRuler className="text-green-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Units</p>
+            <p className="font-medium">{data.units}</p>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaCalendarAlt className="text-teal-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">First Measurement</p>
+            <p className="font-medium">
+              {data.firstMeasurementTime?.toLocaleDateString()}
+            </p>
+            <p className="text-sm text-gray-500">
+              {data.firstMeasurementTime?.toLocaleTimeString()}
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaClock className="text-orange-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Last Measurement</p>
+            <p className="font-medium">
+              {data.lastMeasurementTime?.toLocaleDateString()}
+            </p>
+            <p className="text-sm text-gray-500">
+              {data.lastMeasurementTime?.toLocaleTimeString()}
+            </p>
+          </div>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaChartLine className="text-purple-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Min Value</p>
+            <p className="font-medium">
+              {data.minValue !== null && data.minValue !== undefined
+                ? formatNumber(data.minValue)
+                : 'N/A'}
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaChartBar className="text-red-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Max Value</p>
+            <p className="font-medium">
+              {data.maxValue !== null && data.maxValue !== undefined
+                ? formatNumber(data.maxValue)
+                : 'N/A'}
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaChartPie className="text-yellow-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Average Value</p>
+            <p className="font-medium">
+              {data.avgValue !== null && data.avgValue !== undefined
+                ? formatNumber(data.avgValue)
+                : 'N/A'}
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+          <FaChartBar className="text-indigo-500 text-2xl mr-3" />
+          <div>
+            <p className="text-sm text-gray-500">Count</p>
+            <p className="font-medium">{data.count}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MeasurementSummary;
