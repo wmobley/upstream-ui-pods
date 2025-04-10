ce60fede2006b78b2ff42dd2ec19dbfabd9cb00e
Maximiliano Osorio <maxiosorio@gmail.com>
2025-04-10 14:36:52 -0400

    fix: ununsed imports

--- src/app/TimeSeriesGraph/TimeSeriesGraph.tsx
+++ src/app/TimeSeriesGraph/TimeSeriesGraph.tsx
@@ -1,9 +1,4 @@
-import TimeSeriesChart from '../ScatterTimeSeriesChart';
-import { useState } from 'react';
-import { useList } from '../../hooks/measurements/useList';
 import QueryWrapper from '../common/QueryWrapper';
-import { DataPoint } from '../../utils/dataProcessing';
-import { formatNumber } from '../common/NumberFormatter/NumberFortatterUtils';
 import MeasurementSummary from '../SensorDashboard/_components/MeasurementSummary';
 import Chart from './_components/Chart';
 import { useDetail } from '../../hooks/sensor/useDetail';
