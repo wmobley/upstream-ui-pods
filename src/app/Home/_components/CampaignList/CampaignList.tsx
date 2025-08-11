43238b8d8433781541f41a1815694f190b07266b
hvarg <hern.var@gmail.com>
2025-08-11 09:53:40 -0400

    styling

--- src/app/Home/_components/CampaignList/CampaignList.tsx
+++ src/app/Home/_components/CampaignList/CampaignList.tsx
@@ -76,7 +76,7 @@ const CampaignList: React.FC = () => {
     >
       <section className="mx-auto max-w-screen-xl px-4 lg:px-8 flex flex-col gap-10">
         <div className='breadcrumbs'>
-          <a href='#'>Explore campaigns</a>
+          <a href='#' className='active'>Explore campaigns</a>
         </div>
         <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
           Explore campaigns
